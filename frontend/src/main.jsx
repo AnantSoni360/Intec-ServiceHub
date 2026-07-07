1f0cce82097173dc60f7ffcfde8eb4fc6c7e01b9
AnantSoni360 <147417339+AnantSoni360@users.noreply.github.com>
2026-07-07 16:30:44 +0530

    chore: update google oauth client id

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "871526383060-0f2gngmo5jj5qk4comuegt76vimftlr5.apps.googleusercontent.com"}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "460888150980-g5gri4vd4m4f4ra0csv83f7rq6sq9n5l.apps.googleusercontent.com"}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
